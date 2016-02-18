af29fc8054d2ae0928cbe95026557b5e24ad47a6
Martin Schwitalla <martin.schwitalla@zalando.de>
2016-02-18 10:05:49 +0100

    feat(auth-lib): added IT for getTokenInfo
    Close #10

---
+++ integration-test/oauth/get-token-info.spec.ts
@@ -0,0 +1,137 @@
+'use strict';
+
+import * as chai from 'chai';
+import * as chaiAsPromised from 'chai-as-promised';
+import * as HttpStatus from 'http-status';
+import * as Express from 'express';
+import * as Http from 'http';
+import * as fetch from 'node-fetch';
+
+import {
+  handleOAuthRequestMiddleware,
+  PASSWORD_CREDENTIALS_GRANT
+} from '../../src/oauth-tooling';
+
+chai.use(chaiAsPromised);
+const expect = chai.expect;
+
+describe('Integration tests for getTokenInfo', () => {
+
+  let authenticationServer: Http.Server;
+  let resourceServer: Http.Server;
+  let authServerApp: Express.Application;
+
+  // Setup API server
+  beforeEach(() => {
+    let app = Express();
+
+    app.use(handleOAuthRequestMiddleware({
+      publicEndpoints: [ '/public', '/healthcheck' ],
+      tokenInfoEndpoint: 'http://127.0.0.1:30001/oauth2/tokeninfo'
+    }));
+
+    resourceServer = app.listen(30002);
+  });
+
+  // Setup AuthServer
+  beforeEach(() => {
+    authServerApp = Express();
+    authenticationServer = authServerApp.listen(30001);
+  });
+
+  // stop server after test
+  afterEach(() => {
+    resourceServer.close();
+    authenticationServer.close();
+  });
+
+  function addStandardAuthenticationEndpoint() {
+
+    authServerApp.get('/oauth2/tokeninfo', function(req, res) {
+      let valid = req.query.access_token === '4b70510f-be1d-4f0f-b4cb-edbca2c79d41';
+
+      if (valid) {
+        res
+        .status(200)
+        .send({
+          'expires_in': 3515,
+          'token_type': 'Bearer',
+          'realm': 'employees',
+          'scope': [
+            'campaign.editall',
+            'campaign.readall'
+          ],
+          'grant_type': PASSWORD_CREDENTIALS_GRANT,
+          'uid': 'services',
+          'access_token': '4b70510f-be1d-4f0f-b4cb-edbca2c79d41'
+        });
+      } else {
+        res
+          .status(200)
+          .send({
+            'error': 'invalid_request',
+            'error_description': 'Access Token not valid'
+          })
+      }
+    });
+  }
+
+  it('should return error if token is not valid', () => {
+
+    // given
+    let authToken = 'invalid'
+    addStandardAuthenticationEndpoint();
+
+    // when
+    const url = 'http://127.0.0.1:30001/oauth2/tokeninfo?access_token=' + authToken;
+    let promise = fetch(url, {
+      method: 'GET'
+    })
+    .then((res: any) => {
+      return res.json();
+    })
+    .then((jsonData) => {
+      return jsonData;
+    });
+
+    // then
+    return expect(promise).to.become({
+        'error': 'invalid_request',
+        'error_description': 'Access Token not valid'
+      });
+  });
+
+
+  it('should return the token info if token is valid', function() {
+
+    // given
+    let authToken = '4b70510f-be1d-4f0f-b4cb-edbca2c79d41'
+    addStandardAuthenticationEndpoint();
+
+    // when
+    const url = 'http://127.0.0.1:30001/oauth2/tokeninfo?access_token=' + authToken;
+    let promise = fetch(url, {
+      method: 'GET'
+    })
+    .then((res: any) => {
+      return res.json();
+    })
+    .then((jsonData) => {
+      return jsonData;
+    });
+
+    // then
+    return expect(promise).to.become({
+      'access_token': '4b70510f-be1d-4f0f-b4cb-edbca2c79d41',
+      'expires_in': 3515,
+      'grant_type': 'password',
+      'realm': 'employees',
+      'scope': [
+        'campaign.editall',
+        'campaign.readall'
+      ],
+      'token_type': 'Bearer',
+      'uid': 'services'
+    });
+  });
+});
