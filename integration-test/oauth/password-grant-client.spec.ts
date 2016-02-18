37a42de4341e330fd2182ee3ed20fb5b74065ae9
Martin Schwitalla <martin.schwitalla@zalando.de>
2016-02-18 13:25:14 +0100

    feat(oAuth-lib): added integration tests for the authorization code workflow
    Close #12

--- integration-test/oauth/password-grant-client.spec.ts
+++ integration-test/oauth/password-grant-client.spec.ts
@@ -4,32 +4,51 @@ import * as chai from 'chai';
 import * as chaiAsPromised from 'chai-as-promised';
 import * as Express from 'express';
 import * as Http from 'http';
+import * as fetch from 'node-fetch';
+import * as bodyParser from 'body-parser';
 
 import {
   getAccessToken,
+  createAuthCodeRequestUri,
   PASSWORD_CREDENTIALS_GRANT,
-  SERVICES_REALM } from '../../src/oauth-tooling';
+  AUTHORIZATION_CODE_GRANT,
+  SERVICES_REALM
+} from '../../src/oauth-tooling';
 
 chai.use(chaiAsPromised);
 const expect = chai.expect;
 
 // Setup API server
 function setupTestEnvironment(authHeader: string, authServerApp: Express.Application) {
+  authServerApp.use(bodyParser.urlencoded({ extended: true }));
   authServerApp.post('/oauth2/access_token', function(req, res) {
-    let valid = req.headers['authorization'] === authHeader;
-    if (valid) {
-      res
-        .status(200)
-        .send({ 'access_token': '4b70510f-be1d-4f0f-b4cb-edbca2c79d41' });
+    if(req.body.grant_type === PASSWORD_CREDENTIALS_GRANT){
+      let valid = req.headers['authorization'] === authHeader;
+      if (valid) {
+        res
+          .status(200)
+          .send({ 'access_token': '4b70510f-be1d-4f0f-b4cb-edbca2c79d41' });
+      } else {
+        res
+          .status(401)
+          .send('Unauthorized');
+      }
     } else {
-      res
-        .status(401)
-        .send('Unauthorized');
+      if(req.body.code && req.body.redirect_uri && req.headers['authorization'] === authHeader){
+        res
+          .status(200)
+          .send({ 'access_token': '4b70510f-be1d-4f0f-b4cb-edbca2c79d41' })
+      } else {
+        res
+          .status(401)
+          .send('Unauthorized');
+      }
     }
+
   });
 }
 
-describe('getAccessToken', () => {
+describe('Integration tests for getAccessToken', () => {
 
   let authenticationServer: Http.Server;
   let authServerApp: Express.Application;
@@ -48,60 +67,129 @@ describe('getAccessToken', () => {
   });
 
   // set up getAccessToken options object
-  before(() => {
-    getAccessTokenOptions = {
-      realm: SERVICES_REALM,
-      scopes: ['campaing.edit_all', 'campaign.read_all'],
-      accessTokenEndpoint: 'http://127.0.0.1:30001/oauth2/access_token',
-      credentialsDir: 'integration-test/data/credentials',
-      grantType: PASSWORD_CREDENTIALS_GRANT
-    };
-  });
 
-  it('should become the access token', function() {
+  describe('password oAuth flow', () => {
+    let getAccessTokenOptions
+    before(() => {
+       getAccessTokenOptions = {
+        realm: SERVICES_REALM,
+        scopes: ['campaing.edit_all', 'campaign.read_all'],
+        accessTokenEndpoint: 'http://127.0.0.1:30001/oauth2/access_token',
+        credentialsDir: 'integration-test/data/credentials',
+        grantType: PASSWORD_CREDENTIALS_GRANT
+      };
+    });
 
-    //given
-    setupTestEnvironment('Basic c3R1cHNfY2FtcC1mcm9udGVuZF80NTgxOGFkZC1jNDdkLTQ3MzEtYTQwZC1jZWExZmZkMGUwYzk6Nmk1Z2hCI1MyaUJLKSVidGI3JU14Z3hRWDcxUXIuKSo=', authServerApp);
+    it('should become the access token', function() {
 
-    //when
-    let bearer = getAccessToken(getAccessTokenOptions)
-      .then((data) => {
-        return data;
-      });
+      //given
+      setupTestEnvironment('Basic c3R1cHNfY2FtcC1mcm9udGVuZF80NTgxOGFkZC1jNDdkLTQ3MzEtYTQwZC1jZWExZmZkMGUwYzk6Nmk1Z2hCI1MyaUJLKSVidGI3JU14Z3hRWDcxUXIuKSo=', authServerApp);
 
-    //then
-    return expect(bearer).to.become({ accessToken: '4b70510f-be1d-4f0f-b4cb-edbca2c79d41' });
-  });
-
-  it('should return error message if authorization header is invalid', function() {
-
-    //given
-    setupTestEnvironment('invalid', authServerApp);
-
-    //when
+      //when
       let bearer = getAccessToken(getAccessTokenOptions)
         .then((data) => {
           return data;
         });
 
-    //then
-    return expect(bearer).to.eventually.haveOwnProperty('error')
+      //then
+      return expect(bearer).to.become({ accessToken: '4b70510f-be1d-4f0f-b4cb-edbca2c79d41' });
+    });
+
+    it('should return error message if authorization header is invalid', function() {
+
+      //given
+      setupTestEnvironment('invalid', authServerApp);
+
+      //when
+        let bearer = getAccessToken(getAccessTokenOptions)
+          .then((data) => {
+            return data;
+          });
+
+      //then
+      return expect(bearer).to.eventually.haveOwnProperty('error')
+    });
+
+    it('should return error message if credentials can not be read', function() {
+
+      //given
+      setupTestEnvironment('invalid', authServerApp);
+
+      //when
+      let bearer = getAccessToken(Object.assign({}, getAccessTokenOptions, {
+          credentialsDir: 'integration-test/data/not-existing'
+      }))
+        .then((data) => {
+          return data;
+        });
+
+      //then
+      return expect(bearer).to.eventually.haveOwnProperty('error')
+    });
+
   });
 
-  it('should return error message if credentials can not be read', function() {
+  describe('authorization_code oAuth flow', () => {
+    let getAccessTokenOptionsAuthorization;
+    before(() => {
+       getAccessTokenOptionsAuthorization = {
+        realm: SERVICES_REALM,
+        scopes: ['campaing.edit_all', 'campaign.read_all'],
+        accessTokenEndpoint: 'http://127.0.0.1:30001/oauth2/access_token',
+        credentialsDir: 'integration-test/data/credentials',
+        code: 'blabla',
+        redirectUri: 'http://127.0.0.1:30001/oauth2/access_token',
+        grantType: AUTHORIZATION_CODE_GRANT
+      };
+    });
 
-    //given
-    setupTestEnvironment('invalid', authServerApp);
+    it('should become the access token', function() {
 
-    //when
-    let bearer = getAccessToken(Object.assign({}, getAccessTokenOptions, {
-        credentialsDir: 'integration-test/data/not-existing'
-    }))
-      .then((data) => {
-        return data;
-      });
+      //given
+      setupTestEnvironment('Basic c3R1cHNfY2FtcC1mcm9udGVuZF80NTgxOGFkZC1jNDdkLTQ3MzEtYTQwZC1jZWExZmZkMGUwYzk6Nmk1Z2hCI1MyaUJLKSVidGI3JU14Z3hRWDcxUXIuKSo=', authServerApp);
 
-    //then
-    return expect(bearer).to.eventually.haveOwnProperty('error')
+      //when
+      let bearer = getAccessToken(getAccessTokenOptionsAuthorization)
+        .then((data) => {
+          return data;
+        });
+
+      //then
+      return expect(bearer).to.become({ accessToken: '4b70510f-be1d-4f0f-b4cb-edbca2c79d41' });
+    });
+
+    it('should return error message if authorization header is invalid', function() {
+
+      //given
+      setupTestEnvironment('invalid', authServerApp);
+
+      //when
+        let bearer = getAccessToken(getAccessTokenOptionsAuthorization)
+          .then((data) => {
+            return data;
+          });
+
+      //then
+      return expect(bearer).to.eventually.haveOwnProperty('error')
+    });
+
+    it('should return error message if credentials can not be read', function() {
+
+      //given
+      setupTestEnvironment('invalid', authServerApp);
+
+      //when
+      let bearer = getAccessToken(Object.assign({}, getAccessTokenOptionsAuthorization, {
+          credentialsDir: 'integration-test/data/not-existing'
+      }))
+        .then((data) => {
+          return data;
+        });
+
+      //then
+      return expect(bearer).to.eventually.haveOwnProperty('error')
+    });
+
   });
+
 });
