fe91d7a43692f760108563d16eab96dc5c73ab75
Bastian Sieker <bastian.sieker@zalando.de>
2016-04-22 14:21:58 +0200

    chore(project): use typings instead of tsd
    Now typings/ can be added to .gitignore.
    Documentation is adapted, accordingly.
    
    see #32

--- typings/form-urlencoded/form-urlencoded.d.ts
+++
@@ -1,9 +0,0 @@
-interface formUrlencodedInterface{
-  (value: string): string;
-}
-
-declare var formUrlencodedVar: formUrlencodedInterface;
-
-declare module "form-urlencoded" {
-  export = formUrlencodedVar;
-}
