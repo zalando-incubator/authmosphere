5afd68dbbe28afc718e574cbba44e2ad0c6cb765
Bastian Sieker <bastian.sieker@zalando.de>
2017-05-05 11:58:05 +0200

    chore(cleanup): remove props from interfaces
    * remove `id_token` from `Token`
    * remove `open_id` from `TokenInfo`
    
    see #52

--- src/types/Token.ts
+++ src/types/Token.ts
@@ -1,7 +1,6 @@
 interface Token {
   access_token: string;
   expires_in: number;
-  id_token: string;
   scope: string;
   token_type: string;
 }
--- src/types/TokenInfo.ts
+++ src/types/TokenInfo.ts
@@ -2,7 +2,6 @@ interface TokenInfo {
   access_token: string;
   expires_in: number;
   grant_type?: string;
-  open_id?: string;
   scope: String[];
   token_type?: string;
   uid?: string;
