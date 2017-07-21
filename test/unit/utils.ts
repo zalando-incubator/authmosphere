b0fa74488fcc9219ec2f09e2fd11ea3a5bc5368a
Markus Goerlich <markus.goerlich@zalando.de>
2017-07-21 14:19:47 +0200

    chore(cleanup): Added unit test
    close #85

---
+++ test/unit/utils.ts
@@ -0,0 +1,20 @@
+import * as chai from 'chai';
+import * as chaiAsPromised from 'chai-as-promised';
+
+import { getFileData } from '../../src/utils';
+
+chai.use(chaiAsPromised);
+const expect = chai.expect;
+
+  describe('getFileData', () => {
+    it('should ignore tailing / in filepath', () => {
+      const promise = Promise.all([
+        getFileData('test/unit/credentials', 'user.json'),
+        getFileData('test/unit/credentials/', 'user.json')
+      ])
+      .then((credentials) => {
+        return credentials[0] === credentials[1];
+        });
+      return expect(promise).to.become(true);
+    });
+  });
