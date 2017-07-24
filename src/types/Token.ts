1d92b415f7d4f0ebcc5e6f769d025477f4504c10
André Waltert <andre.waltert@zalando.de>
2017-07-24 21:19:02 +0200

    chore(Token): Make token extensible

--- src/types/Token.ts
+++ src/types/Token.ts
@@ -1,10 +1,19 @@
-interface Token {
+type CustemTokenPart<T> = {
+  [P in keyof T]: T[P]
+};
+
+type StaticTokenPart = {
   access_token: string;
   expires_in?: number;
   scope?: string[];
   token_type?: string;
   local_expiry?: number;
-  [key: string]: number | string | string[] | undefined;
 }
 
-export { Token };
+type Token<T = {}> = CustemTokenPart<T> & StaticTokenPart;
+
+export {
+  StaticTokenPart,
+  CustemTokenPart,
+  Token,
+ };
