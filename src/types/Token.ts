68fb2739ea547fc469c6ab6ff30dd7b5461689b7
André Waltert <andre.waltert@zalando.de>
2017-07-26 09:42:25 +0200

    chore(token.ts): Remove unnecessary type declaration

--- src/types/Token.ts
+++ src/types/Token.ts
@@ -1,19 +1,14 @@
-type CustomTokenPart<T> = {
-  [P in keyof T]: T[P]
-};
-
 type StaticTokenPart = {
   access_token: string;
   expires_in?: number;
   scope?: string[];
   token_type?: string;
   local_expiry?: number;
-}
+};
 
-type Token<T = any> = CustomTokenPart<T> & StaticTokenPart;
+type Token<T = any> = T & StaticTokenPart;
 
 export {
   StaticTokenPart,
-  CustomTokenPart,
-  Token,
+  Token
  };
