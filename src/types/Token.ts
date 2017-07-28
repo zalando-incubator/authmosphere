a3c499ea59a907739626d1eced1fe6b7773d58cf
André Waltert <awaltert@users.noreply.github.com>
2017-07-28 15:39:15 +0200

    chore(token.ts): Add type declaration (#88)
    * chore(token.ts): Add type declaration
    
    Close #87

--- src/types/Token.ts
+++ src/types/Token.ts
@@ -1,10 +1,11 @@
-interface Token {
+type Token<CustomTokenPart = any> = CustomTokenPart & {
   access_token: string;
   expires_in?: number;
   scope?: string[];
   token_type?: string;
   local_expiry?: number;
-  [key: string]: any;
-}
+};
 
-export { Token };
+export {
+  Token
+ };
