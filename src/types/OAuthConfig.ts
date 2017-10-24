052e7ed56d992f2623cbada36788b82ee9af34de
André Waltert <andre.waltert@zalando.de>
2017-10-24 11:45:51 +0200

    chore(OAuthConfig): Remove unnecessary comments

--- src/types/OAuthConfig.ts
+++ src/types/OAuthConfig.ts
@@ -1,8 +1,8 @@
 import { OAuthGrantType } from '../types';
 
 type GrantConfigBase = {
-  credentialsDir: string; // according to validateOAuthConfig
-  accessTokenEndpoint: string; // according to validateOAuthConfig
+  credentialsDir: string;
+  accessTokenEndpoint: string;
   queryParams?: { [index: string]: string };
 };
 
@@ -13,8 +13,8 @@ type ClientCredentialsGrantConfig = GrantConfigBase & {
 
 type AuthorizationCodeGrantConfig = GrantConfigBase & {
   grantType: OAuthGrantType.AUTHORIZATION_CODE_GRANT;
-  code: string; // according to validateOAuthConfig
-  redirectUri: string; // according to validateOAuthConfig
+  code: string;
+  redirectUri: string;
   scopes: string[];
 };
 
@@ -25,7 +25,7 @@ type PasswordCredentialsGrantConfig = GrantConfigBase & {
 
 type RefreshGrantConfig = GrantConfigBase & {
   grantType: OAuthGrantType.REFRESH_TOKEN_GRANT;
-  refreshToken: string; // according to validateOAuthConfig
+  refreshToken: string;
   scopes: string[];
 };
 
