afdcfa9a8619c0be4c39a22fd9353d086aa0364d
Bastian Sieker <bzums@users.noreply.github.com>
2018-01-05 11:46:38 +0100

    127 improve error handling (#140)
    * refactor(error-handling): consistent api
    
    Errors which are passed outside of this lib should look
    consistent.
    
    close #127

--- test/unit/safe-logger.ts
+++ test/unit/safe-logger.ts
@@ -1,7 +1,7 @@
 import * as chai from 'chai';
 import * as chaiAsPromised from 'chai-as-promised';
 
-import { logOrNothing } from '../../src';
+import { safeLogger } from '../../src';
 import { Logger } from '../../src/types';
 
 chai.use(chaiAsPromised);
@@ -16,20 +16,20 @@ describe('safeLogger', () => {
         info: () => {
           called = true;
         }
-      };
+      } as any as Logger;
 
-      const result = () => logOrNothing.info('foo', logger);
+      const result = () => safeLogger(logger).info('foo');
       expect(result).not.to.throw();
       return expect(called).to.be.true;
     });
 
     it('should not throw, if logger undefined', () => {
-      const result = () => logOrNothing.info('foo', undefined);
+      const result = () => safeLogger(undefined).info('foo');
       return expect(result).not.to.throw();
     });
 
     it('should not throw, if logger does not fulfill Logger interface', () => {
-      const result = () => logOrNothing.info('foo', {} as Logger);
+      const result = () => safeLogger({} as Logger).info('foo');
       return expect(result).not.to.throw();
     });
   });
