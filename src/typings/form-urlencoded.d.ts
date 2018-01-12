01579a99eabcb38b66cdb5804511f8413ed4823b
Markus Goerlich <markus.goerlich@zalando.de>
2018-01-12 14:03:12 +0100

    feat(body scopes): Get rid of package url-formencoded
    close #153

--- src/typings/form-urlencoded.d.ts
+++
@@ -1,9 +0,0 @@
-interface formUrlencodedInterface {
-  (obj: object, options?: object): string;
-}
-
-declare const formUrlencodedVar: formUrlencodedInterface;
-
-declare module "form-urlencoded" {
-  export = formUrlencodedVar;
-}
