09622f3e509e9dc17d365f42a8bedb5b59f3a974
Aleksei Bezrodnov <aleksei.bezrodnov@zalando.de>
2023-02-21 10:15:00 +0100

    chore(287): solution without use of prototypes

--- src/safe-logger.ts
+++ src/safe-logger.ts
@@ -1,26 +1,28 @@
-import { Logger, LogFunction } from './types';
+import { Logger, LogFunction, LogLevel } from './types';
 
 const voidLogging: LogFunction = () => undefined;
 
-const loggerOrNoop = (logfunction?: LogFunction): LogFunction =>
-  logfunction ? logfunction : voidLogging;
+const isLogFunction = (value: unknown): value is LogFunction =>
+  typeof value === 'function';
 
-const safeLogger = (logger?: Logger): Logger => {
-  const _safeLogger = {
-    ...logger,
-    info: loggerOrNoop(logger?.info),
-    debug: loggerOrNoop(logger?.debug),
-    error: loggerOrNoop(logger?.error),
-    fatal: loggerOrNoop(logger?.fatal),
-    trace: loggerOrNoop(logger?.trace),
-    warn: loggerOrNoop(logger?.warn),
-  };
-
-  if (logger) {
-    Object.setPrototypeOf(_safeLogger, Object.getPrototypeOf(logger));
+const loggerOrNoop = (logLevel: LogLevel, logger?: Logger): LogFunction => {
+  if (logger && isLogFunction(logger[logLevel])) {
+    // not returning here `logger[logLevel]` to keep logger context (this)
+    // alternative solution could be returning `logger[logLevel].bind(logger)`
+    return (message: string, ...args: unknown[]) =>
+      logger[logLevel](message, ...args);
   }
-
-  return _safeLogger;
+  return voidLogging;
 };
+
+const safeLogger = (logger?: Logger): Logger => ({
+  ...logger,
+  info: loggerOrNoop(LogLevel.info, logger),
+  debug: loggerOrNoop(LogLevel.debug, logger),
+  error: loggerOrNoop(LogLevel.error, logger),
+  fatal: loggerOrNoop(LogLevel.fatal, logger),
+  trace: loggerOrNoop(LogLevel.trace, logger),
+  warn: loggerOrNoop(LogLevel.warn, logger),
+});
 
 export { safeLogger };
